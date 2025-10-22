bca9fd0293f4be9e6b59ad7436c8bba223e78a6a
Jafe <marlonmarques6989@gmail.com>
2025-10-22 20:38:42 -0300

    Atualizacao automatica em 22/10/2025 20:38

--- git-push-automático.bat
+++ git-push-automático.bat
@@ -13,7 +13,7 @@ git add .
 :: Cria commit com data e hora
 set hora=%time:~0,2%:%time:~3,2%
 set data=%date%
-git commit -m "Atualização automática em %data% %hora%"
+git commit -m "Atualizacao automatica em %data% %hora%"
 
 :: Faz o push pro GitHub
 git push
